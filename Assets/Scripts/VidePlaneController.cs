32de18e28af88125b4d5993144b31db433525a69
jordanbae <73390197+jordanbae@users.noreply.github.com>
2021-08-04 19:49:40 +0700

    Added Image Target

---
+++ Assets/Scripts/VidePlaneController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidePlaneController : MonoBehaviour
+{
+    [SerializeField] GameObject videoPlane;
+    Animation m_animation;
+    Animator m_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animation = videoPlane.GetComponent<Animation>();
+        m_animator = videoPlane.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnImageTracked(){
+        m_animator.SetTrigger("moveup");
+    }
+    
+    public void OnImageLostTracked(){
+        VideoPlaneLocation();
+    }
+
+    public void VideoPlaneLocation(){
+        videoPlane.transform.localRotation = Quaternion.identity;
+        videoPlane.transform.localPosition = new Vector3(0.0f, -0.5f, 0.0f);
+    }
+}
